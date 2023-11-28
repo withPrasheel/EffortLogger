2b15ee617e8999e5c139c9d7128e9026fa441c5e
Prasheel <prasheelnandwana3@gmail.com>
2023-11-27 20:33:01 -0700

    test fxml and controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,17 +2,19 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("./view/MainScene.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setTitle("Hello World!");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- src/module-info.java
+++
@@ -1,5 +0,0 @@
-module EffortLogger {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
