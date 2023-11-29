11b4da2fe656c2c6fb78048e129b08ba8b674195
mikeluigi64 <mkofman@asu.edu>
2023-11-28 18:52:47 -0700

    Buttons added properly
    why it wasn't added lmao

--- src/application/controller/LoginSignupController.java
+++ src/application/controller/LoginSignupController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -17,6 +18,8 @@ public class LoginSignupController {
 	
 	private Stage primaryStage;
 	
+	@FXML Button viewStory;
+	
 	
 	//Add close logic
 
@@ -28,4 +31,6 @@ public class LoginSignupController {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
+	
+	
 }
