920bdfb341b085bfaef6dbf1a61591924f8eda52
Prasheel <prasheelnandwana3@gmail.com>
2023-11-30 10:08:06 -0700

    Estimate card added

---
+++ src/application/controller/EstimateCardSceneController.java
@@ -0,0 +1,15 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class EstimateCardSceneController {
+	@FXML
+	private Label estStPoints;
+	@FXML
+	private Label estTime;
+	
+
+}
